527544c266ba3efd98da703624ccdb8d243a2bd2
TisonK <33223860+TisonK@users.noreply.github.com>
2017-10-30 10:37:04 -0400

    Update TransReader.java

--- TransReader.java
+++ TransReader.java
@@ -1,4 +1,4 @@
-package com.chapter4.pa;
+//package TransProcessor;
 
 import java.util.Scanner;
 import java.io.*;
@@ -9,7 +9,7 @@ public class TransReader
 
 {
 	public enum Operators			
-	{				DLM,FRE,RDI,MSR,MRI			}
+	{				DLM,FRE,RDI,MSR,MRI,MDB		}
 	
 	
 	
@@ -116,10 +116,22 @@ public class TransReader
 				case "RDI":
 					String num1 = lineScanner.next();
 					String num2 = lineScanner.next();
-					int int1;
-					int int2;
+					int int1 = 0;
+					int int2 = 0;
+					//Added try and catch for num1 10/30/17
+					try {
 					int1 = Integer.parseInt(num1);
+					}
+					catch(NumberFormatException e) {
+						System.out.println("Invalid Parameter: " + num1);
+					}
+					//Added try and catch for num2 10/30/17
+					try {
 					int2 = Integer.parseInt(num2);
+					}
+					catch(NumberFormatException e) {
+						System.out.println("Invalid Parameter: " + num2);
+					}
 					int randomNum = int1 + Gen.nextInt(int2);
 					System.out.println(randomNum);
 					break;
@@ -141,12 +153,35 @@ public class TransReader
 					 {
 						double sqrtNum = Math.sqrt(Dub);
 						System.out.println(sqrtNum);
+						break;
+					}
+				//Added case MDB 10/30/17	
+				case "MDB":
+					String dub1 = lineScanner.next();
+					String dub2 = lineScanner.next();
+					double input3 = 0;
+					double input4 = 0;
+					//Added try and catch for dub1 10/30/17
+					try {
+					input3 = Double.parseDouble(dub1);
+					}
+					catch(NumberFormatException e) {
+						
+						 System.out.println("Invalid parameter: " + dub1);
+					}
+					//Added try and catch for dub2 10/30/17
+					try {
+					input4 = Double.parseDouble(dub2);
 					}
 					
-				case "MDB":
-					
-				
-				
+					catch(NumberFormatException e) {
+						 
+						 System.out.println("Invalid parameter: " + dub2);
+						 
+						 
+					 }
+					System.out.println(input3 * input4);
+					break;
 				
 				}		
 				
