de357d255845d20d6a8143e82635d81bda51f041
TisonK <33223860+TisonK@users.noreply.github.com>
2017-11-01 08:35:19 -0400

    Update TransDriver.java

--- TransDriver.java
+++ TransDriver.java
@@ -1,14 +1,25 @@
-package com.chapter4.pa;
+package TransProcessor;
 
 import java.io.File;
-
+import java.io.IOException;
+import java.util.Scanner;
 public class TransDriver
 {
 
-	public static void main(String[] args)
+	public static void main(String[] parms) throws IOException
 	{
+		
+		String NAME = "";
+		Scanner scan = new Scanner(System.in);
+		if (parms.length > 0)
+			NAME = (parms[0]);
+		else
+		{System.out.println("Please enter your name: ");
+		NAME = scan.nextLine();}
+		System.out.println(NAME);
 		TransProj myTrans = new TransProj(new File("C:\\JavaTransfer\\TransProcessor\\Tran1.txt"));
 				myTrans.TransProcessor();
+	scan.close();
 	}
 
 }
